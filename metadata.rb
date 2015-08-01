4e47fb9e56f4e053098e1398379a01776c8638ee
Henrik Højgaard Jensen <hhj@Henriks-MacBook-Retina.local>
2015-08-01 21:41:37 +0200

    Initial

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'dealhunter'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures dealhunter'
+long_description 'Installs/Configures dealhunter'
+version          '0.1.0'
